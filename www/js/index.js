901cae7441a34c680173ad29e70d07b15f5de494
Jon Latorre <jonlatorremartinez@gmail.com>
2014-11-17 15:58:05 +0100

    Comercios en json que se cargan OK y se muestrane ne l mapa

--- www/js/index.js
+++ www/js/index.js
@@ -32,24 +32,56 @@ var bbdd = {
 			console.log("Somos createBD");
 			console.log("Borramos la BBDD si existe");
 			tx.executeSql('DROP TABLE IF EXISTS comercios');
-			sql="CREATE TABLE comercios(id INTEGER PRIMARY KEY ASC, nombre TEXT, telefono TEXT,direccion TEXT, lat REAL, lon REAL,categoria INTEGER,  etiqueta_1 INTEGER, etiqueta_2 INTEGER, etiqueta_3 INTEGER, etiqueta_4 INTEGER, etiqueta_5 INTEGER, etiqueta_6 INTEGER);"
-			console.log("Creamos la BBDD");
-			console.log(sql);
+			sql="CREATE TABLE IF NOT EXISTS `comercios` (\
+				`id_comercio` INTEGER PRIMARY KEY AUTOINCREMENT,\
+				`nombre` varchar(255) NOT NULL,\
+				`categoria` varchar(255) NOT NULL,\
+				`direccion` varchar(255) NOT NULL,\
+				`telefono` varchar(15) NOT NULL,\
+				`etiqueta_1` Boolean,\
+				`etiqueta_2` Boolean,\
+				`etiqueta_3` Boolean,\
+				`etiqueta_4` Boolean,\
+				`etiqueta_5` Boolean,\
+				`etiqueta_6` Boolean,\
+				`latitud` varchar(20) NOT NULL,\
+				`longitud` varchar(20) NOT NULL\
+			);"
+			console.log("Creamos la tabla categorias");
 			tx.executeSql(sql);
+			sql = "CREATE TABLE IF NOT EXISTS `categorias` (\
+				`id_categoria` int(11) NOT NULL,\
+				`categoria` varchar(255) NOT NULL\
+			);"
+			console.log("Creamos la tabla categorias");
+			tx.executeSql(sql);
+			
 			bbdd.populateDB();
 		});
 	},
 	// Populate the database 
     populateDB: function () {
-        console.log("Somos populateBD");
-		bbdd.db.transaction(function(tx){
-			for (i = 0; i < 50; i++) {
-				console.log("Añadimos datos");
-				sql = 'INSERT INTO comercios (nombre, categoria, etiqueta_1,etiqueta_2) VALUES ("Comercio '+i+'",'+Math.floor(Math.random()*10)+','+Math.floor(Math.random()*2)+','+Math.floor(Math.random()*2)+')';
-				tx.executeSql(sql);
-			}
-			
-		}); 
+        console.log("Somos populateBD");		
+		console.log("Cargamos el JSON local");
+		$.getJSON( "js/comercios.json",function( data ) {
+			console.log("JSON cargado vamos a recorrerlo");
+				$.each( data, function( key,val ) {
+					var comercio = val;
+					bbdd.db.transaction(function(tx){
+						sql = "INSERT INTO comercios (nombre,categoria,direccion,telefono,latitud,longitud) VALUES ('"+comercio.nombre+"','"+comercio.categoria+"','"+comercio.direccion+"','"+comercio.telefono+"','"+comercio.lat+"','"+comercio.lon+"')";
+						console.log(sql);
+						tx.executeSql(sql,[], 
+							function (tx, results) { console.log('Insert OK'+results); }, 
+							function(tx,error){ console.log("Insert KO: "+error.code+" "+error.message); }
+						);
+				});
+			});
+		}).fail(function(jqxhr, textStatus, error) {
+				console.log( "error" );
+				var err = textStatus + ", " + error;
+				console.log( "Request Failed: " + err );
+		});
+		 
          
     },
     
@@ -86,8 +118,8 @@ var bbdd = {
 	},
     // Transaction error callback
     //
-    errorCB: function (err) {
-        console.log("Error processing SQL: "+err);
+    errorCB: function (tx,err) {
+        console.log("Error processing SQL: "+err.message);
     },
 
     // Transaction success callback
@@ -98,26 +130,14 @@ var bbdd = {
 	refreshDb: function() {
 		
 		console.log("Refresh DB");
-		console.log("Cargamos el JSON del server");
-		var jqxhr = $.getJSON("http://etxea.net/medicus/comercios.json", 
-			function(data) {
-				console.log( "success" );
-				app.refrescarLista(data);
-			})
-			.fail(function(jqxhr, textStatus, error) {
-			console.log( "error" );
-			var err = textStatus + ", " + error;
-			console.log( "Request Failed: " + err );
-			})
-		console.log("Terminado checkUpdates");
-		
+		bbdd.createBD();
 	},
 	listComercios: function(categoria,etiquetas) {
 		console.log("Vamos a buscar en BBDD los comercios con categoria "+categoria+" y las etiquetas "+etiquetas);
 		if (categoria>0) {
 			consulta = "SELECT * FROM comercios WHERE categoria = "+categoria;
 		} else {
-			consulta = "SELECT * FROM comercios WHERE categoria > 0";
+			consulta = "SELECT * FROM comercios WHERE 1=1";
 		}
 		while (etiquetas.length > 0 ) {
 			consulta = consulta + " AND etiqueta_"+etiquetas.pop()+"=1";
@@ -137,7 +157,7 @@ var bbdd = {
 						comercio = results.rows.item(i);
 						//console.log("Añadimos "+comercio.id+" "+comercio.nombre);
 						$("#listado_comercios").append('<ons-list-item class="list__item ons-list-item-inner"\
-							onclick=\'app.navi.pushPage("ficha.html", { ficha: "True", comercio: "'+comercio.id+'"});\' >'
+							onclick=\'app.navi.pushPage("ficha.html", { ficha: "True", comercio: "'+comercio.id_comercio+'"});\' >'
 							+comercio.nombre+'</ons-list-item>');
 					}
 
@@ -150,7 +170,7 @@ var bbdd = {
 	},
 	cargarComercio: function(id_comercio) {
 		console.log("Vamos a buscar en BBDD el comercio "+id_comercio);
-		consulta = "SELECT * FROM comercios WHERE id = "+id_comercio;
+		consulta = "SELECT * FROM comercios WHERE id_comercio = "+id_comercio;
 		console.log("El sql es: "+consulta);
 		bbdd.db.transaction(function(tx){
 			tx.executeSql(consulta, [],function (tx, results) {
@@ -159,9 +179,8 @@ var bbdd = {
 					comercio = results.rows.item(0);
 					console.log("Añadimos "+comercio.id+" "+comercio.nombre);
 					$("#ficha_comercio_nombre").text(comercio.nombre);
-					$("#ficha_comercio_direccion").text(comercio.nombre);
-					$("#ficha_comercio_telefono").text(comercio.nombre);
-					
+					$("#ficha_comercio_direccion").text(comercio.direccion);
+					$("#ficha_comercio_telefono").text(comercio.telefono);
 				}else{
 					console.log("Sin resultados");
 				}
@@ -227,6 +246,17 @@ var app = {
 	// FUNCIONES MAPA //
 	initializeMap: function () {
 		//~ console.log("Iniciamos el mapa");
+		var myIcon = L.icon({
+					iconUrl: 'img/marker-icon.png',
+					iconRetinaUrl: 'img/marker-icon-2x.png',
+					iconSize: [38, 95],
+					iconAnchor: [22, 94],
+					popupAnchor: [-3, -76],
+					shadowUrl: 'img/marker-shadow.png',
+					shadowRetinaUrl: 'img/marker-shadow.png',
+					shadowSize: [68, 95],
+					shadowAnchor: [22, 94]
+				});
 		alto = $(window).height()
 		alto2 = windowHeight = screen.height; 
 		console.log("Alto ventana: "+alto+" "+alto2);
@@ -236,9 +266,29 @@ var app = {
 		var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 		var osmAttrib = 'Map data © OpenStreetMap contributors';
 		var osm = new L.TileLayer(osmUrl, { attribution: osmAttrib });
-		app.map.setView(new L.LatLng(42.847363,-2.6734835), 14);
+		app.map.setView(new L.LatLng(42.847363,-2.6734835), 13);
 		app.map.addLayer(osm);
-		//~ console.log("Mapa Iniciado");
+		
+		consulta = "SELECT id_comercio,nombre,direccion,latitud,longitud,telefono FROM comercios";
+		bbdd.db.transaction(function(tx){
+			tx.executeSql(consulta, [],function (tx, results) {
+				var len = results.rows.length;
+				console.log("Hemos encontrado "+len+" comercios");		
+				if (len>0) {
+					console.log("Vamos a añadir los marcadores");
+					for (i = 0; i < len; i++) {
+						comercio = results.rows.item(i);
+						//~ console.log("Añadimos "+comercio.id_comercio+" al mapa en ["+comercio.latitud+","+comercio.longitud+"]");
+						popup_texto = "<div onclick='app.navi.pushPage(\"ficha.html\", { ficha: \"True\", comercio: \""+comercio.id_comercio+"\"})';>"+comercio.nombre+" "+comercio.telefono+"</div>";
+						//console.log(popup_texto);
+						L.marker([comercio.longitud, comercio.latitud],{icon: myIcon}).addTo(app.map).bindPopup(popup_texto);
+					}
+					console.log("Marcadores listos");
+				}else{
+					console.log("Sin resultados");
+				}},function(tx,error){console.log("Error leyendo de BBDD: "+error.message)});
+		});
+		console.log("Mapa Iniciado");
 	},
     buscame_onSuccess: function(position) {
         console.log("Te pillé");
@@ -253,7 +303,7 @@ var app = {
         //var map = new L.Map('map');
         console.log("Centremonos");
             
-        app.map.setView(new L.LatLng(position.coords.latitude, position.coords.longitude), 16);
+        app.map.setView(new L.LatLng(position.coords.latitude, position.coords.longitude), 15);
   
         console.log("Centrado");
     },
@@ -334,6 +384,9 @@ var app = {
 			console.log( "Request Failed: " + err );
 			})
 		console.log("Terminado checkUpdates");
+	},
+	refreshDb: function() {
+		bbdd.refreshDb();
 	},
 	
 };
