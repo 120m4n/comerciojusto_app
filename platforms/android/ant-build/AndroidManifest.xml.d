833ed4ae6e4b0a2583e4643610232d8df2083a1d
Jon Latorre <jonlatorremartinez@gmail.com>
2015-02-20 17:01:10 +0100

    Limpiando

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/patataman/comerciojusto_app/platforms/android/ant-build/AndroidManifest.xml : \
-/home/patataman/comerciojusto_app/platforms/android/AndroidManifest.xml \
--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/patataman/comerciojusto_app/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/home/patataman/comerciojusto_app/platforms/android/ant-build/CordovaApp.ap_ \
-/home/patataman/comerciojusto_app/platforms/android/ant-build/classes.dex \
-/home/patataman/comerciojusto_app/platforms/android/CordovaLib/ant-build/classes.jar \
