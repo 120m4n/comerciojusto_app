a6016de3d0a39469c58cc4a614bb5aee7b7667d7
Jon Latorre <jonlatorremartinez@gmail.com>
2015-02-23 17:31:53 +0100

    limpiando el platforms

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/patataman/comerciojusto_app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/patataman/comerciojusto_app/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/com.phonegap.plugins.facebookconnect/FacebookLib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/raul/comerciojusto_app/platforms/android/com.phonegap.plugins.facebookconnect/FacebookLib/ant-build/AndroidManifest.xml : \
-/home/raul/comerciojusto_app/platforms/android/com.phonegap.plugins.facebookconnect/FacebookLib/AndroidManifest.xml \
